00ffcf9c28f6b5825c31a650c589e1bbc4fb6082
Dmitry Prokudin <prodemix@gmail.com>
2025-11-11 22:47:06 +0300

    minor changes

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -7,7 +7,7 @@ public class ProductsPage extends BasePage {
     // Sauce Labs Backpack
     private static final String ADD_TO_CART =
             "//*[text()='%s']//ancestor:: div[@class='inventory_item']//child::button[text()='Add to cart']";
-    private By title = By.xpath("//*[@data-test='title']");
+    private final By title = By.xpath("//*[@data-test='title']");
 
     public ProductsPage(WebDriver driver) {
         super(driver);
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class FirstClass extends BaseTest {
+public class LoginTest extends BaseTest {
     @Test
     public void validLogin() {
         loginPage.open();
