9e6a6eb71c694e2128a177634b4ea6f75d8a886d
Dmitry Prokudin <prodemix@gmail.com>
2025-11-11 22:00:08 +0300

    Added Test checkGoodsAddedToCart()

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProductsPage extends BasePage {
-
+    // Sauce Labs Backpack
+    private static final String ADD_TO_CART =
+            "//*[text()='%s']//ancestor:: div[@class='inventory_item']//child::button[text()='Add to cart']";
     private By title = By.xpath("//*[@data-test='title']");
 
     public ProductsPage(WebDriver driver) {
@@ -18,9 +20,11 @@ public class ProductsPage extends BasePage {
     public String getTitleText() {
         return driver.findElement(title).getText();
     }
-//    assertTrue(productsTitlePresent, "Ожидалось наличие названия страницы 'Products'");
 
-
+    public void addToCart(final String goodsName) {
+        By addToCart = By.xpath(ADD_TO_CART.formatted(goodsName));
+        driver.findElement(addToCart).click();
+    }
 
 
 }
--- src/test/java/tests/FirstClass.java
+++ src/test/java/tests/FirstClass.java
@@ -20,4 +20,11 @@ public class FirstClass extends BaseTest {
         loginPage.login("locked_out_user", "secret_sauce");
         assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Sorry, this user has been locked out.");
     }
+
+    @Test
+    public void checkGoodsAddedToCart() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        productsPage.addToCart("Sauce Labs Bike Light");
+    }
 }
