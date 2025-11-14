8730ec5e7020dc99d5f32d960cb2c41b4197af73
Dmitry Prokudin <prodemix@gmail.com>
2025-11-14 16:46:59 +0300

    added 3 more test-data lines in @DataProvider - loginData() deleted related 3 test-methods

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -21,36 +21,17 @@ public class LoginTest extends BaseTest {
         return new Object[][]{
                 {"locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."},
                 {"", "secret_sauce", "Epic sadface: Username is required"},
-                {"standard_user", "", "Epic sadface: Password is required"}
+                {"standard_user", "", "Epic sadface: Password is required"},
+                {"", "", "Epic sadface: Username is required"},
+                {"123", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"},
+                {"standard_user", "12345678", "Epic sadface: Username and password do not match any user in this service"}
         };
     }
-
 
     @Test(dataProvider = "loginData")
     public void incorrectLogin(String user, String password, String errorMessage) {
         loginPage.open();
         loginPage.login(user, password);
         assertEquals(loginPage.checkErrorMsg(), errorMessage);
-    }
-
-    @Test
-    public void EmptyLoginAndPassword() {
-        loginPage.open();
-        loginPage.login("", "");
-        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Username is required");
-    }
-
-    @Test
-    public void IncorrectLoginValidPassword() {
-        loginPage.open();
-        loginPage.login("123", "secret_sauce");
-        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Username and password do not match any user in this service");
-    }
-
-    @Test
-    public void ValidLoginIncorrectPassword() {
-        loginPage.open();
-        loginPage.login("standard_user", "12345678");
-        assertEquals(loginPage.checkErrorMsg(), "Epic sadface: Username and password do not match any user in this service");
     }
 }
